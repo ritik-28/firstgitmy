01464abc26c3a464c0559d5c7630e878663b685c
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-03-07 11:36:44 +0530

    reponsesTOpageUSINGurlANDwithoutUsingURL

--- nodejs-1/app.js
+++ nodejs-1/app.js
@@ -1,7 +1,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("ritik");
+  // console.log("ritik");
+  // process.exit();
+  // console.log(req.url, req.method, req.headers);
+  if (req.url === "/home") {
+    res.write("<h1>welcome home</h1>");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("<h1>welcome to About Us page</h1>");
+    res.end();
+  } else if (req.url === "/node") {
+    res.write("<h1>welcome to my nodejs project</h1>");
+    res.end();
+  }
+  // res.setHeader("content-type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>my first page</title></head>");
+  // res.write("<body><h1>Hello from my nodejs server</h1></body>");
+  // res.write("</html>");
+  // res.end();
 });
 
 server.listen(4000);
