c47862ded064c734a91f5a0468389510dc66e0a3
ritik-28 <106630315+ritik-28@users.noreply.github.com>
2023-03-09 17:09:51 +0530

    routingANDbodyPARSING

--- expressjs-1/app.js
+++ expressjs-1/app.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+// app.use("/", (req, res, next) => {
+//   console.log("this always runs");
+//   next();
+// });      //we are not sending anything from here so we can move to another code, no error here  that response is already sent
+
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in the add product middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='range' name='size'><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("in the another middleware");
-  res.send("<h1>hello from Express! </h1>"); //content-type = text/html
-  // res.send({ name: "ritik" });   //content-type = application/json
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  console.log("in  product middleware");
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in the / middleware");
+  // res.send("<h1>hello from  / middleware</h1>");
 });
 
 //creating server and also listening now in one line using express
