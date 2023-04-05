7b0651c1d9b7773b52c373758cf5f27e92dd6ccc
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-04-05 12:01:07 +0530

    bcryptPasswordSafety

--- expenseTrackerFull/app.js
+++ expenseTrackerFull/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const sequelize = require("./model/database");
 const User = require("./model/user");
@@ -28,12 +29,14 @@ app.post("/signup", async (req, res, next) => {
     ) {
       res.status(400).json({ err: "fill all feilds" });
     } else {
-      const data = await User.create({
-        name: username,
-        email,
-        password,
+      bcrypt.hash(password, 10, async (err, hash) => {
+        await User.create({
+          name: username,
+          email,
+          password: hash,
+        });
+        return res.status(201).json(`succesfully created new user`);
       });
-      return res.status(201).json(`succesfully created new user`);
     }
   } catch (err) {
     res.status(403).json({ err: err });
@@ -55,17 +58,15 @@ app.post("/login", async (req, res, next) => {
     });
 
     if (emailExists != null) {
-      const passwordtrue = await User.findOne({
-        where: {
-          email,
-          password,
-        },
+      bcrypt.compare(password, emailExists.password, (err, result) => {
+        if (err) {
+          res.status(500).json("something went wrong");
+        } else if (result === true) {
+          res.json("user login succesful");
+        } else {
+          res.status(401).json("User not authorized");
+        }
       });
-      if (passwordtrue != null) {
-        res.json("user login succesful");
-      } else {
-        res.status(401).json("User not authorized");
-      }
     } else {
       res.status(404).json("user not found");
     }
